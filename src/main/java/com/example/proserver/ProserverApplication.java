32ee742d2ebaa797ae18a3411e90cac7cce9190f
Vlad Giscov <v.giscov@tg.dunice.net>
2024-07-25 10:10:41 +0300

    init git

---
+++ src/main/java/com/example/proserver/ProserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.proserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proserver/ProserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
